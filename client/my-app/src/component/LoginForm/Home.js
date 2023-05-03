181c35412a0fc9afeff568a8bbfc686b146be179
RameshKumal <rameshkc498@gmail.com>
2023-05-03 11:52:14 +0530

    Added order page

--- client/my-app/src/component/LoginForm/Home.js
+++ client/my-app/src/component/LoginForm/Home.js
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
+import CartPage from "../AddProduct/Product";
 
 const Dashboard = (props) => {
   const [store, setStore] = useState(null);
   const [variants, setVariant] = useState([]);
+  const [isBought, setBought] = useState([]);
+  const [isOrder, setOrder] = useState(true);
   const location = useLocation();
   const token = location.state?.id;
 
@@ -64,56 +67,68 @@ const Dashboard = (props) => {
     );
     const data = await response.json(); //getting object in response
     console.log(data);
-    // if (data) {
-    //   setVariant([data]); //converting into array.
-    // } else {
-    //   setVariant([]);
-    // }
+    if (data) {
+      setBought([data]); //converting into array.
+      setOrder(false);
+    } else {
+      setVariant([]);
+    }
   };
+  const cancelHandler = () =>{
+    setOrder(true)
+  }
   return (
     <>
-      <select onChange={onOptionChange}>
-        <option value="0">---select product---</option>
-        {store?.map((item) => (
-          <option key={item.product_id} value={item.product_id}>
-            {item.productName}
-          </option>
-        ))}
-      </select>
-      <br></br>
-      <h3>Various product variants </h3>
-      <br></br>
-      <table className="table table-hover bg-se">
-        <thead>
-          <tr>
-            <th>Variant Id</th>
-            <th>Variant Name</th>
-            <th>Price</th>
-            <th>options</th>
-            <th>order</th>
-          </tr>
-        </thead>
-        <tbody>
-          {variants.length > 0 ? (
-            variants?.map((variant) => (
-              <tr key={variant.variant_id}>
-                <td>{variant.variant_id}</td>
-                <td>{variant.title}</td>
-                <td>{variant.price}</td>
-                <td>{variant.option}</td>
-                <td>
-                  <button onClick={clickHandler} value={variant.variant_id}>
-                    Buy Order
-                  </button>
-                </td>
+      {isOrder ? (
+        <>
+          <select onChange={onOptionChange}>
+            <option value="0">---select product---</option>
+            {store?.map((item) => (
+              <option key={item.product_id} value={item.product_id}>
+                {item.productName}
+              </option>
+            ))}
+          </select>
+          <br></br>
+          <h3>Various product variants </h3>
+          <br></br>
+          <table className="table table-hover bg-se">
+            <thead>
+              <tr>
+                <th>Variant Id</th>
+                <th>Variant Name</th>
+                <th>Price</th>
+                <th>options</th>
+                <th>order</th>
               </tr>
-            ))
-          ) : (
-            <div>No data found</div>
-          )}
-        </tbody>
-      </table>
-      {/* <UserDashboard userValue ={store}/>  */}
+            </thead>
+            <tbody>
+              {variants.length > 0 ? (
+                variants?.map((variant) => (
+                  <tr key={variant.variant_id}>
+                    <td>{variant.variant_id}</td>
+                    <td>{variant.title}</td>
+                    <td>{variant.price}</td>
+                    <td>{variant.option}</td>
+                    <td>
+                      <button onClick={clickHandler} value={variant.variant_id}>
+                        Buy Order
+                      </button>
+                    </td>
+                  </tr>
+                ))
+              ) : (
+                <div>No data found</div>
+              )}
+            </tbody>
+          </table>
+        </>
+      ) : (
+        <div>
+          <CartPage order={isBought}/>
+          <button onClick={cancelHandler}>Back to Home</button>
+        </div>
+      )}
     </>
   );
 };
--- server/controllers/order.js
+++ server/controllers/order.js
@@ -1,38 +1,41 @@
-const db = require('../db/index');
+const db = require("../db/index");
 const Store = db.store;
 const Product = db.product;
 const Variant = db.variant;
-const Order = db.order
+const Order = db.order;
 
-const buyOrder = async(req, res) =>{
-    try{
-        const store_id = req.cookies["id"];
-        const variant_id = req.params.id;
+const buyOrder = async (req, res) => {
+  try {
+    const store_id = req.cookies["id"];
+    const variant_id = req.params.id;
 
-        const storeTable = await Store.findOne({where:{store_id: store_id}});
-        const variantTable = await Variant.findOne({where:{variant_id:variant_id}});
+    const storeTable = await Store.findOne({ where: { store_id: store_id } });
+    const variantTable = await Variant.findOne({
+      where: { variant_id: variant_id },
+    });
 
-        const product_id = variantTable.product_id;
-        const productTable = await Product.findOne({where:{product_id:product_id}});
+    const product_id = variantTable.product_id;
+    const productTable = await Product.findOne({
+      where: { product_id: product_id },
+    });
 
-        if(storeTable && variantTable && productTable){
-            const order = await Order.create({
-                storeOwner: storeTable.storeOwner,
-                product_name:  productTable.productName,
-                variant_name:  variantTable.title,
-                price:  variantTable.price,
-            });
+    if (storeTable && variantTable && productTable) {
+      const order = await Order.create({
+        storeOwner: storeTable.storeOwner,
+        product_name: productTable.productName,
+        variant_name: variantTable.title,
+        price: variantTable.price,
+      });
 
-            res.status(200).json(order);
-        }else{
-            res.status(400).json({error:"Cannot able to import the data."})
-        }
-
-    }catch(err){
-        res.status(500).json({message: err.message});
+      res.status(200).json(order);
+    } else {
+      res.status(400).json({ error: "Cannot able to import the data." });
     }
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 };
 
-module.exports={
-    buyOrder
-}
+module.exports = {
+  buyOrder,
+};
